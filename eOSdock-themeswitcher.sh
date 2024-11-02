245f17ae931a7b719d47db3612238f34cd428985
Ryo Nakano <ryonakaknock3@gmail.com>
2024-11-02 11:25:37 +0900

    Improve coding style
    - Fix indentation
    - Add .editorconfig

--- eOSdock-themeswitcher.sh
+++ eOSdock-themeswitcher.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Manage dock themes
 
 #========================
-#   	VARIABLES		=
+#		VARIABLES		=
 #========================
 
 
@@ -12,18 +12,18 @@ IMPORTFILE="Docktheme.css"
 
 
 #========================
-#	   	FUNCTIONS   	=
+#		FUNCTIONS		=
 #========================
 
 
 # add an import to gtk.css so it can apply stuff
 function enable_theme()
 {
-	if ! grep -q "@import \"$IMPORTFILE\";" ~/.config/gtk-4.0/gtk.css 
+	if ! grep -q "@import \"$IMPORTFILE\";" ~/.config/gtk-4.0/gtk.css
 	then
 		echo "import line not present in gtk.css. Adding."
-		echo '/* Import line for the eOS dock theme switcher. Comment for vanilla. */' >> ~/.config/gtk-4.0/gtk.css	
-	    echo "@import \"$IMPORTFILE\";" >> ~/.config/gtk-4.0/gtk.css	
+		echo '/* Import line for the eOS dock theme switcher. Comment for vanilla. */' >> ~/.config/gtk-4.0/gtk.css
+		echo "@import \"$IMPORTFILE\";" >> ~/.config/gtk-4.0/gtk.css
 	fi
 	#killall io.elementary.dock
 }
@@ -38,16 +38,16 @@ function disable_theme()
 {
 
 	echo "removing import line in gtk.css."
-	
-	
+
+
 	# Cant seem to chain directly to itself
 	cat ~/.config/gtk-4.0/gtk.css \
-	| grep -v  "@import \"$IMPORTFILE\";" \
-	| grep -v "/* Import line for the eOS dock theme switcher." \
-	> ~/.config/gtk-4.0/gtk1.css
+		| grep -v  "@import \"$IMPORTFILE\";" \
+		| grep -v "/* Import line for the eOS dock theme switcher." \
+		> ~/.config/gtk-4.0/gtk1.css
 
 	# Use the temporary, then delete
-	cat ~/.config/gtk-4.0/gtk1.css > ~/.config/gtk-4.0/gtk.css	
+	cat ~/.config/gtk-4.0/gtk1.css > ~/.config/gtk-4.0/gtk.css
 	rm -f ~/.config/gtk-4.0/gtk1.css
 
 	rm -vf ~/.config/gtk-4.0/$IMPORTFILE
@@ -57,23 +57,23 @@ function disable_theme()
 
 
 #========================
-#   	EXECUTION   	=
+#		EXECUTION		=
 #========================
 
 
 if [[ -f "$*" ]] && [[ "$*" != "Default" ]]		# Its a file and not "Default" (reserve Default, just in case)
 then
 
-    echo "setting $*"
-	echo "/* CURRENT: $* */" 	> ~/.config/gtk-4.0/$IMPORTFILE
-	echo "" 					>> ~/.config/gtk-4.0/$IMPORTFILE
-	cat "$*"  					>> ~/.config/gtk-4.0/$IMPORTFILE
+	echo "setting $*"
+	echo "/* CURRENT: $* */"	>  ~/.config/gtk-4.0/$IMPORTFILE
+	echo ""						>> ~/.config/gtk-4.0/$IMPORTFILE
+	cat "$*"					>> ~/.config/gtk-4.0/$IMPORTFILE
 	enable_theme
 
 
 else
 	disable_theme
-	
+
 
 
 fi
